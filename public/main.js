f2fa6242c3a6dd5990ae7254de4fa77556e46113
Mark Hamilton <ham.mark126@gmail.com>
2022-11-08 20:13:58 -0600

    couple of imports

--- public/main.js
+++ public/main.js
@@ -1,34 +1,6 @@
 // import '../styles/main.scss'; // You have to import your styles for them to work. Comment in this line
-
-const students = [];
-const voldysArmy = [];
-const houses = [
-  {
-    house: 'gryffindor',
-    crest:
-      'https://static.wikia.nocookie.net/pottermore/images/1/16/Gryffindor_crest.png'
-  },
-  {
-    house: 'slytherin',
-    crest:
-      'https://static.wikia.nocookie.net/pottermore/images/4/45/Slytherin_Crest.png'
-  },
-  {
-    house: 'hufflepuff',
-    crest:
-      'https://static.wikia.nocookie.net/pottermore/images/5/5e/Hufflepuff_crest.png'
-  },
-  {
-    house: 'ravenclaw',
-    crest:
-      'https://static.wikia.nocookie.net/pottermore/images/4/4f/Ravenclaw_crest.png'
-  }
-];
-
-const renderToDOM = (divId, content) => {
-  const selectedDiv = document.querySelector(divId);
-  selectedDiv.innerHTML = content;
-};
+import { students, voldysArmy, houses } from '../utils/data/studentData';
+import renderToDOM from '../utils/renderToDom';
 
 // ********** HTML Components  ********** //
 // the basic HMTL structure of app
